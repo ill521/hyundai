6fb229b095c58acba2be171fb73eca08b3442bb9
ill521 <83938020+ill521@users.noreply.github.com>
2021-07-22 10:12:54 +0900

    hyundai-4
    메인 슬라이더 미흡

--- js/mainslide.js
+++
@@ -1,7 +0,0 @@
-$('.mainSlider').slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear'
-});
--- js/mainslider.js
+++ js/mainslider.js
@@ -3,5 +3,7 @@ $('.mainSlider').slick({
   infinite: true,
   speed: 500,
   fade: true,
-  cssEase: 'linear'
+  cssEase: 'linear',
+  prevArrow: $('#arrow_prev'),
+  nextArrow: $('#arrow_next'),
 });
